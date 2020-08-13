985a6c825397dddf22877886e7811b849c2245e8
Dmitriy Volkov <37198050+WheatleyHDD@users.noreply.github.com>
2020-08-13 21:36:22 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import sys
+
+def generate(file, output):
+	s = open(file, "rt")
+	s = s.read()
+	#s = "Тест\\Тест1\\0" + '\n' + "Привет\\Здряв\\0" + '\n' + "Привет\\Здряв https://vk.com/photo405436173_456239223\\0"
+	
+	for i in range(0,4):
+		s = s.replace('\\'+str(i), '|')
+	
+	s = s.replace('\\', '|')
+	
+	#print(len("https://vk.com"))
+	#print(len("http://vk.com/"))
+	
+	arr = s.split('\n')
+	for k,v in enumerate(arr):
+		phase = v.split('|')
+		links = []
+		phaseOtvet = phase[1].split(' ')
+	
+		for i, word in enumerate(phaseOtvet):
+			if len(word) > 14:
+				if word[:14] == "https://vk.com" or word[:14] == "http://vk.com/":
+					links.append(word)
+					phaseOtvet.pop(i)
+		
+		phase[1] = ' '.join(phaseOtvet)
+		
+		for i, link in enumerate(links):
+			link = link.replace("https://vk.com/", "")
+			link = link.replace("http://vk.com/", "")
+			
+			links[i] = link
+			
+		phase[2] = ",".join(links)
+		stroka = '|'.join(phase)
+		arr[k] = stroka
+	
+	s = "\n".join(arr)
+	
+	#print(s)
+	f = open(output, "w")
+	f.write(s)
+	print("Готово!")
+
+
+if len(sys.argv) == 2:
+	generate(sys.argv[1], "PhasesDB.txt")
+elif len(sys.argv) >= 3:
+	generate(sys.argv[1], sys.argv[2])
+else:
+	print("Ошибка! Вы не указали файл!")
